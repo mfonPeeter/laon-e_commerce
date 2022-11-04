d4059f6e3b155cd20a24fc96846a673d4cf3084a
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2022-11-04 05:23:57 +0100

    style: fixed title placement in videos section

--- src/components/HomePage/Videos/ProductVideo.js
+++ src/components/HomePage/Videos/ProductVideo.js
@@ -9,12 +9,12 @@ const ProductVideo = ({ img, showIframeModalHandler, title, extraTitle }) => {
       >
         <img src={img} alt="Redmi Note 10 Series" />
         <div className="flex flex-col items-center space-y-3 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full">
-          <button className="flex justify-center items-center w-16 h-16 bg-white opacity-30 rounded-full transition-opacity duration-500 group-hover:opacity-80 md:w-20 md:h-20 lg:w-24 lg:h-24">
+          <button className="flex justify-center items-center w-16 h-16 bg-white opacity-20 rounded-full transition-opacity duration-500 group-hover:opacity-80 md:w-20 md:h-20 lg:w-24 lg:h-24">
             <PlayIcon />
           </button>
           <span className="px-6 text-center text-gray-100">
             {title}
-            <span className="sm:hidden lg:block">{extraTitle}</span>
+            <span className="sm:hidden lg:inline">{extraTitle}</span>
           </span>
         </div>
       </div>
