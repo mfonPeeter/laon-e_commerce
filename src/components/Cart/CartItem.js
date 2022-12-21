6a9644231bd8f14907ef9a85c3c8bb7e543bf60c
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2022-12-22 00:03:11 +0100

    feat: attached a link to the products in the cart page leading to their respective specification

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 import SmallLoadingSpinner from '../UI/SmallLoadingSpinner';
 import CartContext from '../../store/cart-context';
@@ -8,27 +9,32 @@ const CartItem = ({ item }) => {
 
   const {
     line_total: { formatted_with_symbol: formattedWithSymbol },
+    product_id: productId,
   } = item;
+
+  console.log(item);
 
   return (
     <div className="p-2 mb-2 bg-white rounded lg:px-6">
-      <div className="flex space-x-1 mb-2 md:space-x-4">
-        <div className="w-1/3 sm:w-1/5 md:w-1/6">
-          <img src={item.image.url} alt={item.name} className="w-full " />
+      <Link reloadDocument to={`/products/${productId}`}>
+        <div className="flex space-x-1 mb-2 md:space-x-4">
+          <div className="w-1/3 sm:w-1/5 md:w-1/6">
+            <img src={item.image.url} alt={item.name} className="w-full " />
+          </div>
+          <div className="w-10/12">
+            <h4>{item.name}</h4>
+            <h4 className="font-bold text-lg">{formattedWithSymbol}</h4>
+            <p className="text-sm text-gray-700">In Stock</p>
+            <p className="text-md font-lora font-semibold uppercase">
+              <span className="font-semibold text-blue-800">Laon</span>Express
+            </p>
+            <p className=" max-w-sm text-sm text-gray-700 leading-right">
+              Laon Express items in your order will be delivered for free
+              (Lagos, Ibadan & Abuja only, exluding large items)
+            </p>
+          </div>
         </div>
-        <div className="w-10/12">
-          <h4>{item.name}</h4>
-          <h4 className="font-bold text-lg">{formattedWithSymbol}</h4>
-          <p className="text-sm text-gray-700">In Stock</p>
-          <p className="text-md font-lora font-semibold uppercase">
-            <span className="font-semibold text-blue-800">Laon</span>Express
-          </p>
-          <p className=" max-w-sm text-sm text-gray-700 leading-right">
-            Laon Express items in your order will be delivered for free (Lagos,
-            Ibadan & Abuja only, exluding large items)
-          </p>
-        </div>
-      </div>
+      </Link>
 
       <div className="flex items-center justify-between">
         <div className="flex">
@@ -67,7 +73,7 @@ const CartItem = ({ item }) => {
             onClick={() =>
               cartCtx.updateCartQtyHandler(item.id, item.quantity + 1)
             }
-            className="w-8 h-8 bg-blue-700 text-white text-3xl rounded shadow-[1px_4px_8px_0.5px_rgba(0,0,0,0.3)] transition-colors flex outline-blue-900 hover:bg-blue-800"
+            className="w-8 h-8 bg-blue-700 text-white text-3xl rounded shadow-[1px_4px_8px_0.5px_rgba(0,0,0,0.3)] transition-colors outline-blue-900 hover:bg-blue-800"
           >
             <span className="flex items-center justify-center w-full h-full sm:items-end">
               &#43;
